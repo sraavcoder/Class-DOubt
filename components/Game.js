c572346afaf563136117e83f0ff8ea2c4cc9a088
Sraav Gamer <sraav.3@gmail.com>
2021-06-18 19:42:03 +0530

    second

--- components/Game.js
+++ components/Game.js
@@ -2,6 +2,12 @@ AFRAME.registerComponent("game-play", {
     schema: {
       elementId: { type: "string", default: "#ring1" },
     },
+
+    init: function (){
+      var duration = 120
+      var timerEl = document.querySelector("#timer")
+      this.startTimer(duration, timerEl);
+    },
   
     update: function () {
       this.isCollided(this.data.elementId);
@@ -11,10 +17,62 @@ AFRAME.registerComponent("game-play", {
       const element = document.querySelector(elementId);
       element.addEventListener("collide", (e) => {
         if (elementId.includes("#ring")) {
-          console.log(elementId + " collision");
+          elementId
+          this.updateScore();
+          this.updateTargets();
         } else if (elementId.includes("#hurdle")) {
-          console.log("bird collision");
+          this.gameOver()
         }
+      });
+    },
+
+    startTimer: function(duration, timerEl){
+      var minutes, seconds;
+      setInterval(()=>{
+        if (duration >= 0){
+          minutes = parseInt(duration/60);
+          seconds = parseInt(duration%60);
+
+          if(minutes<10){
+            minutes = "0"+minutes;
+          }
+          if(seconds<10){
+            seconds = "0"+seconds;
+          }
+
+          timerEl.setAttribute("text", {value:minutes + ":" + seconds})
+          duration -= 1
+        }
+        else{
+          this.gameOver();
+        }
+      },1000)
+    },
+
+    updateTargets: function () {
+      var element = document.querySelector("#targets");
+      var count = element.getAttribute("text").value;
+      var currentTargets = parseInt(count);
+      currentTargets -= 1;
+      element.setAttribute("text", {
+        value: currentTargets,
+      });
+    },
+    updateScore: function () {
+      var element = document.querySelector("#score");
+      var count = element.getAttribute("text").value;
+      var currentScore = parseInt(count);
+      currentScore += 50;
+      element.setAttribute("text", {
+        value: currentScore,
+      });
+    },
+    gameOver: function () {
+      var planeEl = document.querySelector("#plane_model");
+      var element = document.querySelector("#game_over_text");
+      element.setAttribute("visible", true);
+      planeEl.setAttribute("dynamic-body", {
+        mass: 1
       });
     },
   });
